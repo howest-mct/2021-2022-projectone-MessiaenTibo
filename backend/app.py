61bc1a374eb12527982e7655962267ed7a74c06c
tibomessiaen <tibo.messiaen#student.howest.be>
2022-05-27 14:07:33 +0200

    Temperatuur inlezen + juiste user weergeven

--- backend/app.py
+++ backend/app.py
@@ -1,27 +1,75 @@
+#region **** IMPORTS ****
 import json
 from flask_socketio import SocketIO, emit, send
-#from importlib_metadata import method_cache
 from pkg_resources import require
 from repositories.DataRepository import DataRepository
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from datetime import datetime, date
 import threading
+from RPi import GPIO
+#endregion
+
+
+#region **** INIT ****
 # Start app
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'geheim!'
 socketio = SocketIO(app, cors_allowed_origins="*", logger=False, engineio_logger=False, ping_timeout=1)
 CORS(app)
-# init sensors
-sensor_file_name = '/sys/bus/w1/devices/28-22cf61000900/w1_slave'
-
-
 
 # Custom endpoint
 endpoint = '/api/v1'
 
+# OneWire (GPIO 4)
+sensor_file_name = '/sys/bus/w1/devices/28-22cf61000900/w1_slave'
 
-# ROUTES
+# MagnetContacts
+MagnetContactOne = 18
+MagnetContactTwo = 23
+MagnetContactThree = 24
+MagnetContactFour = 25
+
+MagnetContactOneState = False
+MagnetContactTwoState = False
+MagnetContactThreeState = False
+MagnetContactFourState = False
+
+# Humidity sensor
+humiditySensor = 12
+
+# Buzzer
+Buzzer = 18
+
+# Button
+button = 5
+
+# LedCircle
+LedCircle = 26
+
+# Water Flow Sensor
+WaterFlowSensor = 21
+
+#endregion
+
+
+
+#region **** SETUP ****
+def setup():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(MagnetContactOne, GPIO.IN, GPIO.PUD_DOWN)
+    GPIO.setup(MagnetContactTwo, GPIO.IN, GPIO.PUD_DOWN)
+    GPIO.setup(MagnetContactThree, GPIO.IN, GPIO.PUD_DOWN)
+    GPIO.setup(MagnetContactFour, GPIO.IN, GPIO.PUD_DOWN)
+    GPIO.add_event_detect(MagnetContactOne, GPIO.RISING, FindUser, bouncetime=200)
+    GPIO.add_event_detect(MagnetContactTwo, GPIO.RISING, FindUser, bouncetime=200)
+    GPIO.add_event_detect(MagnetContactThree, GPIO.RISING, FindUser, bouncetime=200)
+    GPIO.add_event_detect(MagnetContactFour, GPIO.RISING, FindUser, bouncetime=200)
+    FindUser()
+    Write_temperature()
+
+
+#region **** ROUTES ****
 @app.route('/')
 def hallo():
     print('start')
@@ -55,24 +103,71 @@ def historiekId():
         data = DataRepository.read_historyId()
         return jsonify(data), 200
 
-# Schakeling
-def Inlezen_temperatuur():
-    tekst = ''
+#endregion
+
+
+
+
+
+#region **** METHODS ****
+# OneWire
+def read_temperature():
+    temperature = ''
     sensor_file = open(sensor_file_name,'r')
     for line in sensor_file:
         if 't=' in line: 
-            tekst = line[29] + line[30] + "." + line[31]+ line[32]+ line[33]
-        print(tekst)
+            temperature = line[29] + line[30] + "." + line[31]+ line[32]+ line[33]
+    print(temperature)
+    return temperature
 
-    DataRepository.update_temp(88,2,1, datetime.now() , tekst, "Ingelezen temperatuur")
-    threading.Timer(1,Inlezen_temperatuur).start()
+def Write_temperature():
+    temperature = read_temperature()
+    DataRepository.update_temp(88,2,1, datetime.now() , str(temperature), "Ingelezen temperatuur")
+    threading.Timer(1,Write_temperature).start()
+
+
+
+# Humidity Sensor
   
-        
+
+
+# Water Flow Sensor
+
+
+# Find User
+def FindUser(x=0):
+    SelectedMagnetContact = 0
+    MagnetContactOneState = GPIO.input(MagnetContactOne)
+    MagnetContactTwoState = GPIO.input(MagnetContactTwo)
+    MagnetContactThreeState = GPIO.input(MagnetContactThree)
+    MagnetContactFourState = GPIO.input(MagnetContactFour)
+    if(((MagnetContactOneState ^ MagnetContactTwoState) ^ (MagnetContactThreeState ^ MagnetContactFourState)) & ((MagnetContactOneState & MagnetContactTwoState != 1) & (MagnetContactThreeState & MagnetContactFourState != 1))):
+        if(MagnetContactOneState):
+            SelectedMagnetContact = 1
+        elif(MagnetContactTwoState):
+            SelectedMagnetContact = 2
+        elif(MagnetContactThreeState):
+            SelectedMagnetContact = 3
+        elif(MagnetContactFourState):
+            SelectedMagnetContact = 4
+        print("User {} is selected".format(SelectedMagnetContact))
+    else:
+        if(MagnetContactOneState | MagnetContactTwoState | MagnetContactThreeState | MagnetContactFourState):
+            print("There is only 1 user at the same time allowd!")
+        else:
+            print("There must be at least 1 selected user!")
+        threading.Timer(1,FindUser).start()
+    # print(MagnetContactOneState)
+    # print(MagnetContactTwoState)
+    # print(MagnetContactThreeState)
+    # print(MagnetContactFourState)
+
+#endregion
+
 
 
 # START THE APP
 if __name__ == '__main__':
-    Inlezen_temperatuur()
-    # socketio.run(app, debug=True, host='0.0.0.0')
     print("backend running")
+    setup()
     socketio.run(app, debug=False, host='0.0.0.0')
